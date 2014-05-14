9ed9b574a7b49d1cbf6235bae100d01f68f42a32
Simon de Bakker <simon@simbits.nl>
2014-05-14 21:44:21 +0000

    update cabinet if wired connection is active

--- cabinet.sh
+++ cabinet.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
-CABINET_BIN=/opt/cabinet/cabinet.py
+CABINET_PATH=/opt/lumiere/cabinet
+CABINET_BIN=$CABINET_PATH/cabinet.py
 PYTHON=/usr/bin/python
+
+have_wired_network ()
+{
+    ifconfig eth0 | grep -q inet
+    return $?
+}
+
+update_cabinet ()
+{
+    echo "updating cabinet"
+    pushd $CABINET_PATH > /dev/null
+    git pull origin master
+    popd > /dev/null
+    echo "done"
+}
+
+if have_wired_network
+then
+    update_cabinet
+fi
 
 while :
 do
