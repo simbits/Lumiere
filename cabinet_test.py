fb3658e9947eb10a75a4519cecf9c8ebdafd8246
Simon de Bakker <simon@simbits.nl>
2014-05-26 00:34:29 +0200

    Little cabinet test script

--- cabinet_test.py
+++ cabinet_test.py
@@ -6,27 +6,28 @@ import struct
 import sys
 import time
 
-from Adafruit_MCP230xx import Adafruit_MCP230XX
+#from Adafruit_MCP230xx import Adafruit_MCP230XX
 
 CABINET_VERSION='1.0b'
-START_MSG='## Cabinet version %s ##' % (CABINET_VERSION) 
+START_MSG='## Cabinet version %s ##' % (CABINET_VERSION)
 
 MCAST_GRP = ('224.19.79.1', 9999)
 DRAWERS = 9
 USE_PULLUPS = 1
 WAIT_DELAY = 0.5        #seconds
 
+count = 0
 
 if __name__ == '__main__':
-    mcp = Adafruit_MCP230XX(address=0x20, num_gpios=16) # MCP23017
+    #mcp = Adafruit_MCP230XX(address=0x20, num_gpios=16) # MCP23017
     c_state = [True] * DRAWERS
     p_state = [True] * DRAWERS
     trigger_delay = [0] * DRAWERS
 
-    for i in range(0, DRAWERS):
-        mcp.config(i, mcp.INPUT)
-        mcp.pullup(i, USE_PULLUPS)
-        p_state[i] = bool((mcp.input(i) >> i) & 0x01)
+    #for i in range(0, DRAWERS):
+        #mcp.config(i, mcp.INPUT)
+        #mcp.pullup(i, USE_PULLUPS)
+        #p_state[i] = bool((mcp.input(i) >> i) & 0x01)
 
     print 'initial state: %s' % (str(p_state))
     print 'setting up mcast group @%s' % (str(MCAST_GRP))
@@ -42,17 +43,69 @@ if __name__ == '__main__':
         sys.exit(1)
 
     while True:
-        for i in range(0, DRAWERS):
-            #c_state[i] = not bool((mcp.input(i) >> i) & 0x01)
-            c_state[i] = not bool(random.randint(0,1))
-
-
-        changed = {i for i in range(0, DRAWERS) if c_state[i] != p_state[i]}
-
         try:
-            if len(changed) > 0:
-                print 'sending drawer states %s' % (c_state)
-                sock.sendto('s:%s' % (','.join(['%d' % i for i in c_state])), MCAST_GRP)
+
+            if count == 10:
+                print ''
+                print '-------'
+                print ' i: all drawers closed'
+                print ' e: open drawer 1'
+                print ' p: [1]'
+                print ' r: play movie 1'
+                c_state = [ False, True, True, True, True, True, True, True, True ]
+            if count == 50:
+                print ''
+                print '-------'
+                print ' i: drawer 1 open'
+                print ' e: open drawer 3'
+                print ' p: [1,3]'
+                print ' r: stop movie 1, play movie 3'
+                c_state = [ False, True, False, True, True, True, True, True, True ]
+            if count == 100:
+                print ''
+                print '-------'
+                print ' i: drawer 1, 3 open'
+                print ' e: open drawer 5'
+                print ' p: [1,3,5]'
+                print ' r: stop movie 3, play movie 5'
+                c_state = [ False, True, False, True, False, True, True, True, True ]
+            if count == 150:
+                print ''
+                print '-------'
+                print ' i: drawer 1, 3, 5 open'
+                print ' e: close drawer 5'
+                print ' p: [1, 3]'
+                print ' r: stop movie 5, start random movie from playlist'
+                c_state = [ False, True, False, True, True, True, True, True, True ]
+            if count == 200:
+                print ''
+                print '-------'
+                print ' i: drawer 1, 3 open'
+                print ' e: close all drawers'
+                print ' p: []'
+                print ' r: stop current playing movie, shut off projector'
+                c_state = [ True, True, True, True, True, True, True, True, True ]
+            if count == 250:
+                print ''
+                print '-------'
+                print ' i: all drawers are closed'
+                print ' e: drawer 6, 7, 8 opened'
+                print ' p: [6, 7, 8]'
+                print ' r: enablel projector, start random movie from playlist'
+                c_state = [ True, True, True, True, True, False, False, False, True ]
+            if count == 300:
+                print ''
+                print '-------'
+                print ' i: all drawers are closed'
+                print ' e: drawer 6, 7, 8 opened'
+                print ' p: [6, 7, 8]'
+                print ' r: enablel projector, start random movie from playlist'
+                c_state = [ True, True, True, True, True, False, False, True, True ]
+
+
+
+            print 'sending drawer states %s' % (c_state)
+            sock.sendto('s:%s' % (','.join(['%d' % i for i in c_state])), MCAST_GRP)
         except IndexError:
             pass
         except Exception as e:
@@ -60,4 +113,5 @@ if __name__ == '__main__':
 
         p_state = list(c_state)
 
+        count += 1
         time.sleep(WAIT_DELAY) # relax a little
